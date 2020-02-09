a9d11d68ce0861bd6f2fc6b6ec06f081499c53ec
zdunecki <zdunekhere@gmail.com>
2020-02-09 17:30:13 +0100

    (ux): arrows as buttons

--- src/ArrowNavigation.jsx
+++ src/ArrowNavigation.jsx
@@ -2,22 +2,10 @@ import React from 'react';
 import styled, { css } from 'styled-components';
 
 const RightStyle = css`
-  right: 0;
-  background: rgb(255, 255, 255);
-  background: linear-gradient(
-    90deg,
-    rgba(255, 255, 255, 0) 0%,
-    rgba(0, 0, 0, 0.5) 100%
-  );
+  right: 5px;
 `;
 const LeftStyle = css`
-  left: 0;
-  background: rgb(2, 0, 36);
-  background: linear-gradient(
-    90deg,
-    rgba(0, 0, 0, 0.5) 0%,
-    rgba(255, 255, 255, 0) 100%
-  );
+  left: 5px;
 `;
 const NavigationButton = styled.div`
   justify-content: center;
@@ -25,10 +13,16 @@ const NavigationButton = styled.div`
   display: flex;
   position: absolute;
   bottom: 0;
-  top: 0;
-  width: 25%;
   max-width: 100px;
   cursor: pointer;
+
+  width: 35px;
+  height: 35px;
+  background: rgba(255,255,255,0.9);
+  top: 50%;
+  transform: translateY(-50%);
+  border-radius: 50%;
+
   ${(props) => (props.left ? LeftStyle : RightStyle)};
 `;
 
@@ -43,28 +37,36 @@ const ArrowNavigation = ({
 }) => [
   <NavigationButton
     key="next-nav"
-    onClick={() => (slideCount - 1 > index ? onChangeIndex (index + 1) : onChangeIndex (0))}
+    onClick={(e) => {
+      e.preventDefault ();
+      // eslint-disable-next-line no-unused-expressions
+      (slideCount - 1 > index ? onChangeIndex (index + 1) : onChangeIndex (0));
+    }}
     onMouseLeave={onMouseLeave}
     onMouseEnter={onMouseEnter}
     onTouchStart={onTouchStart}
     onTouchEnd={onTouchEnd}
   >
-    <svg viewBox="0 0 24 24" aria-hidden="true" focusable="false" role="img" fill="currentColor" color="white">
-      <path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z" />
+    <svg width={24} heigt={24} viewBox="0 0 24 24" aria-hidden="true" focusable="false" role="img" fill="#000" color="white">
+      <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z" />
     </svg>
   </NavigationButton>,
   <NavigationButton
     key="before-nav"
     width={[1 / 4]}
-    onClick={() => (index > 0 ? onChangeIndex (index - 1) : onChangeIndex (slideCount - 1))}
+    onClick={(e) => {
+      e.preventDefault ();
+      // eslint-disable-next-line no-unused-expressions
+      (index > 0 ? onChangeIndex (index - 1) : onChangeIndex (slideCount - 1));
+    }}
     onMouseLeave={onMouseLeave}
     onMouseEnter={onMouseEnter}
     onTouchStart={onTouchStart}
     onTouchEnd={onTouchEnd}
     left
   >
-    <svg viewBox="0 0 24 24" aria-hidden="true" focusable="false" role="img" fill="currentColor" color="white">
-      <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z" />
+    <svg width={24} heigt={24} viewBox="0 0 24 24" aria-hidden="true" focusable="false" role="img" fill="#000" color="white">
+      <path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z" />
     </svg>
   </NavigationButton>,
 ];
